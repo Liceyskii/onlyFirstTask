8b75ab1c0e2e57d73f9d2ff0ccc73d853d474bd1
Rabadan Ibragimov <nerazberikha3mail.ru>
2024-07-24 20:45:41 +0300

    init

---
+++ local/components/bitrix/Only/.description.php
@@ -0,0 +1,13 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => 'Учебный компонент',
+	"DESCRIPTION" => '',
+	"SORT" => 20,
+	"PATH" => array(
+		"ID" => "Only",
+	),
+);
+
+?>
